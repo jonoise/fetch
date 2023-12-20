7c35cbf8404f48042312683fa4ee7610c83d9bba
Amilcar Munoz <yellowballpython@gmail.com>
2023-12-19 18:19:15 -0600

    rm events from umami

--- app/territorios/todos/route.ts
+++ app/territorios/todos/route.ts
@@ -1,36 +1,15 @@
 import { NextResponse } from 'next/server'
 import { stateList, cityMap, countyMap } from '../_data/codificacion'
 import { withAuth } from '@/lib/api/withAuth'
-import { getClient } from '@umami/api-client'
-import { defaultPayload } from '@/lib/events/api'
 
 export const GET = withAuth(async ({ req }) => {
   try {
-    const client = getClient({
-      apiEndpoint: process.env.UMAMI_API_CLIENT_ENDPOINT,
-      apiKey: process.env.UMAMI_API_KEY,
-    })
-
-    const { ok, status, data, error } = await client.send({
-      type: 'event',
-      payload: {
-        ...defaultPayload,
-        data: req.headers,
-      },
-    })
-
-    console.log({
-      ok,
-      status,
-      data,
-      error,
-    })
     return NextResponse.json(
       { provincias: stateList, cantones: cityMap, distritos: countyMap },
       { status: 200 }
     )
   } catch (error) {
-    console.log(error)
+    console.log({ error })
     return NextResponse.error()
   }
 })
